c6f21b838ba9aa4edb1c31e51d133648d7c6cab3
Gomicoder17 <202113370@alu.comillas.edu>
2022-09-08 19:49:45 +0200

    Added evaluation function

--- minimax.py
+++ minimax.py
@@ -60,6 +60,62 @@ def is_draw(board):
     return True
 
 
+def evaluate(board, player):
+    score = 0
+    max_score = 0
+    rival = PLAYER1 if player == PLAYER2 else PLAYER2
+    # Traverse board horizontally
+    for row in range(len(board)):
+        for col in range(len(board[0]) - 3):
+            four = [
+                board[row][col],
+                board[row][col + 1],
+                board[row][col + 2],
+                board[row][col + 3],
+            ]
+            if not rival in four:
+                score += 1
+            max_score += 1
+    # Traverse board vertically
+    for row in range(len(board) - 3):
+        for col in range(len(board[0])):
+            four = [
+                board[row][col],
+                board[row + 1][col],
+                board[row + 2][col],
+                board[row + 3][col],
+            ]
+            if not rival in four:
+                score += 1
+            max_score += 1
+
+    # Traverse board diagonally
+    for row in range(len(board) - 3):
+        for col in range(len(board[0]) - 3):
+            four = [
+                board[row][col],
+                board[row + 1][col + 1],
+                board[row + 2][col + 2],
+                board[row + 3][col + 3],
+            ]
+            if not rival in four:
+                score += 1
+            max_score += 1
+
+    for row in range(len(board) - 3):
+        for col in range(3, len(board[0])):
+            four = [
+                board[row][col],
+                board[row + 1][col - 1],
+                board[row + 2][col - 2],
+                board[row + 3][col - 3],
+            ]
+            if not rival in four:
+                score += 1
+            max_score += 1
+    return (2 * score / max_score - 1) * 10
+
+
 def make_move(board, player, x):
     y = len(board) - 1
     while board[y][x] != EMPTY:
@@ -75,15 +131,16 @@ def undo_move(board, x):
 
 
 def minimax(board, depth, player):
+    # print("minimax", board, depth, player)
     winner = check_winner(board)
     if winner:
-        return 1 if winner == player else -1
+        return 1000 if winner == player else -1000
     elif is_draw(board):
         return 0
     elif depth == 0:
-        return r.random() - 0.5  # CHANGE FOR A GOOD EVALUATION FUNCTION
+        return evaluate(board, player)
     elif player == PLAYER1:
-        best = -1
+        best = -1000
         for x in range(len(board[0])):
             if board[0][x] == " ":
                 make_move(board, player, x)
@@ -91,7 +148,7 @@ def minimax(board, depth, player):
                 undo_move(board, x)
         return best
     elif player == PLAYER2:
-        best = 1
+        best = 1000
         for x in range(len(board[0])):
             if board[0][x] == " ":
                 make_move(board, player, x)
@@ -101,20 +158,18 @@ def minimax(board, depth, player):
 
 
 def find_best_move(board, player):
-    best_score = -1
+    best_score = 1000
     best_move = None
     for x in range(len(board[0])):
         if board[0][x] == " ":
             make_move(board, player, x)
-            score = minimax(board, 6, PLAYER1 if player == PLAYER2 else PLAYER2)
-            print(score)
+            score = minimax(board, 0, PLAYER1 if player == PLAYER2 else PLAYER2)
             undo_move(board, x)
-            if score > best_score:
+            if score < best_score:
                 best_score = score
                 best_move = x
     return best_move
 
 
 board = [[EMPTY for i in range(7)] for j in range(6)]
-
 print(find_best_move(board, PLAYER1))
